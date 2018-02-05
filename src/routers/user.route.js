358589f368bca588798ca08550153752c709d8e4
unknown <melvinadrian.purio@gmail.com>
2018-02-05 21:10:55 +0800

    Test Files

--- src/routers/user.route.js
+++ src/routers/user.route.js
@@ -5,6 +5,7 @@ const userModel = require('../schemas/user.schema');
 let userRouter = express.Router();
 
 userRouter.get('/users', (req, res) => {
+  
   userModel.find().then((users) => {
     console.log(users)
     res.send(users);  
@@ -23,8 +24,18 @@ userRouter.get('/users/:id', (req, res) => {
 userRouter.post('/users', (req, res) => {
   console.log(req.body);
   userModel.create({
-    name: req.body.name,
+    Id: req.body.Id,
+    Fname: req.body.Fname,
+    Mname: req.body.Mname,
+    Lname: req.body.Lname,
+    Bday: req.body.Bday,
+    Gender: req.body.Gender,
+    CivilStatus: req.body.CivilStatus,
+    Bplace: req.body.Bplace,
+    MobCellNumber: req.body.MobCellNumber,
     email: req.body.email,
+    Address: req.body.Address,
+    UserName: req.body.UserName,
     password: req.body.password
   }).then((user) => {
     res.send(user);
--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -2,11 +2,53 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-  name: {
+  Id: {
     type: String,
     required: true
   },
+  Fname: {
+    type: String,
+    required: true
+  },
+  Mname: {
+    type: String,
+    required: false
+  },
+  Lname: {
+    type: String,
+    required: true
+  },
+  Bday: {
+    type: String,
+    required: true
+  },
+  Gender: {
+    type: String,
+    required: true
+  },
+  CivilStatus: {
+    type: String,
+    required: true
+  },
+  Bplace: {
+    type: String,
+    required: true
+  },
+  MobCellNumber: {
+    type: String,
+    required: false
+  },
+
   email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  Address: {
+    type: String,
+    required: true
+  },
+  UserName: {
     type: String,
     required: true,
     unique: true
